e92b26f481ea0f6b67944656fa90c2b614092946
Mihai Mandrescu <mihai.mandrescu@gmail.com>
2015-10-16 20:46:05 +0300

    Small changes.

--- metaclass.py
+++ metaclass.py
@@ -1,15 +1,15 @@
 #!/usr/local/bin/python3
 
 class MyBaseClass:
-    x = 10
+    x = 15
 
-variableHoldingADerivedClass = type("MyDerivedClassName",(MyBaseClass,),dict(y=5)) # Class name, class 'parents' and class body as a dictionary
+variableHoldingADerivedClass = type("MyDerivedClassName",(MyBaseClass,),dict(y=10)) # Class name, class 'parents' and class body as a dictionary
 
 x = MyBaseClass()
 y = variableHoldingADerivedClass()
 
-print(x.x)
-print(y.x)
-print(y.y)
+print(x.x+5)
+print(y.x+5)
+print(y.y+5)
 
-# Should be 10 10 5
+# Should be 20 20 15
