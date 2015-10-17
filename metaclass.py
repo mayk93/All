cc8f5990cba0efdb7bf9d4a291f1498f3f763733
Mihai Mandrescu <mihai.mandrescu@gmail.com>
2015-10-17 20:22:33 +0300

    Functions.

--- metaclass.py
+++ metaclass.py
@@ -1,9 +1,14 @@
 #!/usr/local/bin/python3
 
 class MyBaseClass:
-    x = 15
+    x = 150
+    def baseFunction(self,lst,index):
+        try:
+            print(lst[index])
+        except:
+            pass
 
-variableHoldingADerivedClass = type("MyDerivedClassName",(MyBaseClass,),dict(y=10)) # Class name, class 'parents' and class body as a dictionary
+variableHoldingADerivedClass = type("MyDerivedClassName",(MyBaseClass,),dict(y=100,derivedFunction=lambda self,lst,start,end: print(lst[start:end]))) # Class name, class 'parents' and class body as a dictionary
 
 x = MyBaseClass()
 y = variableHoldingADerivedClass()
@@ -12,4 +17,8 @@ print(x.x+5)
 print(y.x+5)
 print(y.y+5)
 
-# Should be 20 20 15
+# Should be 155 155 105
+
+x.baseFunction([1,2,3],0)
+y.baseFunction([4,5,6],1)
+y.derivedFunction([7,8,9,10,11,12],2,5)
