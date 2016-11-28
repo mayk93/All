8ada87460c24322bc80a8d4f4746224cc6eb311c
mayk93 <mihai.mandrescu@gmail.com>
2016-11-28 22:32:40 +0000

    Testing module setup and usage.

---
+++ PythonTests/Python3Tests/PackageTests/LibModule/LibModuleA/LibMAcls.py
@@ -0,0 +1,24 @@
+class LibMacls(object):
+    def __init__(self,
+                 value=0):
+        self.value = 0
+        self.val_gen = self.get_val_gen()
+
+    def get_val_gen(self):
+        initial_value = self.value
+
+        def val_gen(initial_value):
+            while True:
+                yield initial_value
+                initial_value += 1
+
+        return val_gen
+
+    def new_val_gen(self, new_value):
+        old_value = self.value
+        self.value = new_value
+        self.val_gen = self.get_val_gen()
+        self.value = old_value
+
+    def __str__(self):
+        return "Current value is: %s" % self.value
---
+++ PythonTests/Python3Tests/PackageTests/LibModule/LibModuleA/LibMAfct.py
@@ -0,0 +1,6 @@
+def f(x):
+    return x * 2
+
+
+def g(x):
+    return f(x) + 10
