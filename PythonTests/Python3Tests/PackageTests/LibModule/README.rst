3a098649fd8f3325d1df2e8d64865e8ef4c15a81
mayk93 <mihai.mandrescu@gmail.com>
2016-11-28 22:46:02 +0000

    Testing module setup and usage.

---
+++ PythonTests/Python3Tests/PackageTests/LibModule/README.rst
@@ -0,0 +1,4 @@
+A package
+=========
+
+This is a package
---
+++ PythonTests/Python3Tests/PackageTests/LibModule/setup.py
@@ -0,0 +1,24 @@
+# Actual setup tools
+from setuptools import setup, find_packages
+
+import codecs
+import os
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='LibModule',
+    version='1.0.0',
+
+    description='A python library',
+    long_description="".join([line for line in '''
+    This is a python library.
+    '''.strip().split("\n") if len(line) > 0]),
+    classifiers=[
+        'Bad Cls :: Do not publish :: 3.5',
+    ],
+
+    keywords='lib test',
+
+    packages=find_packages(),
+)
