65127b84d392072bc36677342371bc03fe4ab6d2
mayk93 <mihai.mandrescu@gmail.com>
2016-12-08 22:13:54 +0000

    Activity

--- PythonTests/Python3Tests/ActivityScript/activity.py
+++ PythonTests/Python3Tests/ActivityScript/activity.py
@@ -66,12 +66,8 @@ def generate_activity(begin, end, repo_path):
 
 
 def roundTime(dt=None, roundTo=60):
-   """Round a datetime object to any time laps in seconds
-   dt : datetime.datetime object, default now.
-   roundTo : Closest number of seconds to round to, default 1 minute.
-   Author: Thierry Husson 2012 - Use it as you want but don't blame me.
-   """
-   if dt == None : dt = datetime.datetime.now()
+   if dt == None :
+       dt = datetime.datetime.now()
    seconds = (dt.replace(tzinfo=None) - dt.min).seconds
    rounding = (seconds+roundTo/2) // roundTo * roundTo
    return dt + datetime.timedelta(0, rounding-seconds, -dt.microsecond)
